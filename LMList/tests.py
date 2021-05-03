eaff7ecc98b297e1d7e2ab38f0bd474d2ddbd703
leonalyn <leonalyn.maglines@gsfe.tupcavite.edu.ph>
2021-05-04 07:36:38 +0800

    FINAL na TALAGA TO PROMISE

--- LMList/tests.py
+++ LMList/tests.py
@@ -13,21 +13,16 @@ class MyMainPage(TestCase):
    def test_root_url_resolves_to_mainpage_view(self):
       found = resolve('/')
       self.assertEqual(found.func, home_page)
-      
-   
-   
    def test_only_saves_items_when_necessary(self): 
       self.client.get('/')        
       self.assertEqual(Item.objects.count(), 0)
       
 class ListViewTest(TestCase):
- 
    def test_uses_list_template(self):
       list_ = List.objects.create()        
       response = self.client.get(f'/LMList/{list_.id}/')
       self.assertTemplateUsed(response, 'registration.html')
      
-   
    def test_displays_all_list_items(self):        
        list_ = List.objects.create()        
        Item.objects.create(npet='Leonalyn', list=list_)        
@@ -39,26 +34,20 @@ class ListViewTest(TestCase):
        response = self.client.get(f'/LMList/{correct_list.id}/')
        self.assertEqual(response.context['list'], correct_list)  
  
-class NewListTest(TestCase):   
-
- 
+class NewListTest(TestCase):  
    def test_redirects_after_POST(self):        
        response = self.client.post('/LMList/new', data={'pet': 'A new pet','owner': 'A new owner','address':'A new address','breed': 'A new breed','birthday': 'A new birtday'})                     
        new_list = List.objects.first()        
        self.assertRedirects(response, f'/LMList/{new_list.id}/')
        
-
-       
 class NewItemTest(TestCase):
    def test_can_save_a_POST_request_to_an_existing_list(self):       
       other_list = List.objects.create()        
       correct_list = List.objects.create()        
-      
       self.client.post(            
           f'/LMList/{correct_list.id}/add_item',            
           data={'pet': 'A new existing pet','owner': 'A new owner','address':'A new address','breed': 'A new breed','birthday': 'A new birtday'}
           ) 
-      
       self.assertEqual(Item.objects.count(), 1)        
       new_item = Item.objects.first()        
       self.assertEqual(new_item.npet, 'A new existing pet')       
@@ -73,28 +62,21 @@ class NewItemTest(TestCase):
       self.assertRedirects(response, f'/LMList/{correct_list.id}/')
    
 class ORM(TestCase):
-
    def test_saving_and_retrieving_items(self):
       list_ = List()        
       list_.save()
-      
       first_item = Item()        
       first_item.npet = 'The first (ever) list item' 
       first_item.list = list_ 
-      first_item.save()        
-               
+      first_item.save()          
       second_item = Item()      
       second_item.npet = 'Item the second'
       second_item.list = list_         
       second_item.save()
-       
-       
       saved_list = List.objects.first()          
-      self.assertEqual(saved_list, list_)
-                 
+      self.assertEqual(saved_list, list_)       
       saved_items = Item.objects.all()
       self.assertEqual(saved_items.count(), 2)
-       
       first_saved_item = saved_items[0]
       second_saved_item = saved_items[1]      	     
       self.assertEqual(first_saved_item.npet, 'The first (ever) list item')
@@ -121,6 +103,17 @@ def test_displays_only_items_for_that_list(self):
       #self.assertContains(response, 'itemey 2')
       self.assertNotContains(response, 'other list item 1')
       #self.assertNotContains(response, 'other list item 2') '''
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- LMList/views.py
+++ LMList/views.py
@@ -2,30 +2,20 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from LMList.models import Item, List
 
-
-def home_page(request):
-    items = Item.objects.all()
-    return render(request, 'homepage.html',{'items' : items})
-    
-
-
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     return render(request, 'registration.html', {'list': list_})
 
-
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(npet=request.POST['pet'],nBreed =request.POST['breed'],nDay =request.POST['birthday'],list=list_)
-    return redirect(f'/LMList/{list_.id}/')
+def home_page(request):
+    items = Item.objects.all()
+    return render(request, 'homepage.html',{'items' : items})
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(nname =request.POST['owner'],nAddress=request.POST['address'], list=list_)
     return redirect(f'/LMList/{list_.id}/')
 
-
-
-
-
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(npet=request.POST['pet'],nBreed =request.POST['breed'],nDay =request.POST['birthday'],list=list_)
+    return redirect(f'/LMList/{list_.id}/')
